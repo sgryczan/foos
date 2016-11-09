1fede9cf110d2efb34561de0cc54d4b7db1b09ec
Sebastian Gryczan <sebastian.gryczan@gmail.com>
2016-11-09 05:02:10 +0000

    added game logic

--- foosball/game.py
+++ foosball/game.py
@@ -1,20 +1,18 @@
 class Game:
-    def __init__(self, name, id='test', mode='single'):
-        self.name = name
-        self.id = id
-        self.blackScore = 0
-        self.redScore = 0
-        self.mode = mode
+  def __init__(self, name, id='test', mode='single'):
+    self.name = name
+    self.id = id
+    self.blackScore = 0
+    self.redScore = 0
+    self.mode = mode
 
 
-    def get_score(self):
-        currentScore = redScore + " - " + blackScore
-        return currentScore
+  def getScore(self):
+    currentScore = "Red: " + str(self.redScore) + " Black: " + str(self.blackScore)
+    return currentScore
     
-    def scoreRed(self):
- 	self.redScore += 1
-	return
-
-    def scoreBlack(self):
- 	self.blackScore += 1
-	return
+  def score(self, team):
+    if team == "red":
+      self.redScore += 1
+    if team == "black":
+      self.blackScore += 1
--- interface.py
+++ interface.py
@@ -7,23 +7,40 @@ import foosball.game as game
 import foosball.clock as clock
 
 
+DEBUG = True
+
 ser = None
-try:
-    ser = serial.Serial('/dev/ttyACM0', 9600)
-    time.sleep(3)
-    print "----- Connected -----"
+fb = game.Game("testgame")
 
-    while True:
-	try:
-            serial_data = ser.readline()
-            print serial_data
-            sys.stdout.flush()
-        except:
-            pass
+ser = serial.Serial('/dev/ttyACM0', 9600)
+time.sleep(3)
+print "----- Connected -----"
 
-except:
+while True:
+  try:
+    serial_data = ser.readline()
+   
+    # Write to stdout to return data to nodeJS
+    print serial_data
+    if serial_data == "game.startNew":
+      print "Starting new game."
+      fb = None
+      fb = game.Game("testgame")
+    else:
+      event, cmd = serial_data.replace('\r\n','').split(".")
+      if DEBUG:
+        print "event: " + event
+        print "cmd: " + cmd
+        print "cmd is " + str(len(cmd)) + "long"
+      if event == "goal":
+        print "We have a goal"
+        fb.score(cmd)
+        print fb.redScore
+        print fb.blackScore
+        print fb.getScore()
+ 
+    # Flush the STDOUT buffer
+    sys.stdout.flush()
+  except:
     pass
 
-finally:
-    if ser:
-	ser.close()
