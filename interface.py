2c340e1ade617678b4eb01c193228618f836ff63
Sebastian Gryczan <sebastian.gryczan@gmail.com>
2016-11-09 05:36:54 +0000

    fixed buffering issue

--- interface.py
+++ interface.py
@@ -14,10 +14,9 @@ ser = None
 ser = serial.Serial('/dev/ttyACM0', 9600)
 time.sleep(.1)
 print "----- Connected -----"
-dump = ser.readline()
-sys.stdout.flush()
 fb = game.Game("testgame")
 
+ser.flushInput()
 while True:
   try:
     serial_data = ser.readline().replace('\r\n','')
@@ -30,12 +29,12 @@ while True:
       fb = None
       fb = game.Game("testgame")
     else:
-      event, cmd = serial_data.replace('\r\n','').split(".")
+      event, arg = serial_data.replace('\r\n','').split(".")
       if DEBUG:
         print "event: " + event
-        print "cmd: " + cmd
+        print "arg: " + arg
       if event == "goal":
-        fb.score(cmd)
+        fb.score(arg)
         print fb.getScore()
  
     # Flush the STDOUT buffer
