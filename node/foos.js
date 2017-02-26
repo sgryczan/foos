47db867bbb916da958e16e323d8e5858fc6cc972
Sebastian Gryczan <Sebastian Gryczan>
2017-02-26 05:28:32 -0700

    enhancements

--- node/foos.js
+++ node/foos.js
@@ -35,9 +35,15 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function(socket){
+  socket.emit('message', { text : 'Welcome!' });
+
   socket.on('chat message', function(msg){
     console.log(msg);
     io.emit('chat message', msg);
+  });
+  
+  socket.on('subscribe', function (data) {
+    socket.join(data.channel);
   });
 });
 
